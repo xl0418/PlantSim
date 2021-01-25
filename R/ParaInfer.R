7eaa26fa89ec7990521f5eee07cc5ff39854bf32
Liang_Mac <xl0418@gmail.com>
2021-01-25 12:23:25 +0000

    updated

---
+++ R/ParaInfer.R
@@ -0,0 +1,97 @@
+#' Inferring the model parameters
+#'
+#' @param data The data is fitted to infer the model parameters.
+#' @param ngener The number of the generations of the algorithm.
+#' @param nsample The number of the samples for each generation.
+#' @param acc_rate The acceptance rate that determines the best portion of the samples to be accepted.
+
+#' @return The abundance matrix for all plots and species through time.
+#' @examples
+#' PlantSim(nplot = 5, nspe = 2, t = 10, ini_abundance = matrix(1:10, 5, 2),
+#'  growth_rate = 1, interaction_matrix = matrix(0.001, 2, 2), st_portion = 0.7, surv_rate = 1)
+#' @export
+#'
+
+ParaInfer <- function(data, ngener, nsample, acc_rate = 0.25) {
+  load(data)
+  dim_data = dim(plot_abundance)
+  nplot = dim_data[1]
+  nspe = dim_data[2]
+  nt = dim_data[3]
+
+  nparas <- nspe ** 2
+  paras <- array(0, dim = c(ngener, nsample, nparas))
+  fitness <- array(1, dim = c(ngener, nsample))
+
+  for (para_ind in c(1:nparas)) {
+    paras[1, , para_ind] <- runif(nsample, 0, 1)
+  }
+
+
+  for (gener_ind in c(2:ngener)) {
+    print(paste0("Iteration ", gener_ind, "..."))
+    # progress bar
+    pb <-
+      progress::progress_bar$new(format = "[:bar] :current/:total (:percent)", total = nsample)
+    pb$tick(0)
+    Sys.sleep(3)
+    for (sample_ind in c(1:nsample)) {
+      sim_result <- PlantSim(
+        nplot = nplot,
+        nspe = nspe,
+        t = nt,
+        ini_abundance = runif(nspe, 1, 100),
+        growth_rate = 1,
+        interaction_matrix = matrix(paras[gener_ind -
+                                            1, sample_ind, ], nspe, nspe),
+        st_portion = 0.7,
+        surv_rate = 1
+      )
+      fitness[gener_ind, sample_ind] <-
+        1 / sqrt(sum((sim_result - plot_abundance) ** 2))
+      pb$tick(1)
+      Sys.sleep(1 / 100)
+    }
+
+    # pick up the best fit simulations
+    best_fit_index <-
+      sort(fitness[gener_ind, ], index.return = TRUE)$ix
+    best_fit_index_rate <-
+      best_fit_index[floor(nsample * (1 - acc_rate) + 1):nsample]
+
+    best_fit_paras_rate <- paras[gener_ind - 1, best_fit_index_25, ,drop = FALSE]
+    best_fit_rate <- fitness[gener_ind, best_fit_index_25]
+    weight_fit <- best_fit_rate / sum(best_fit_rate)
+
+    # means and vars of the best fit paras
+    #mean_best_fit_paras <- colMeans(best_fit_paras_rate)
+    var_best_fit_paras <- apply(best_fit_paras_rate, 1, var)
+
+    # sample all paras from the best fit paras according to their weights
+    sample_paras <- NULL
+    for (para_ind in c(1:nparas)) {
+      sample_paras <-
+        cbind(
+          sample_paras,
+          sample(
+            best_fit_paras_rate[,, para_ind],
+            nsample,
+            prob = weight_fit,
+            replace = TRUE
+          )
+        )
+    }
+
+    # disturb the samples a bit
+    proposed_paras <- NULL
+    for (para_ind in c(1:nparas)) {
+      proposed_paras <- cbind(proposed_paras,
+                              unlist(
+                                lapply(sample_paras[, para_ind], rnorm, n = 1, sd = var_best_fit_paras[para_ind])
+                              ))
+    }
+
+    # use the new proposed samples
+    paras[gener_ind, , ] <- proposed_paras
+  }
+}
--- R/PlantSim.R
+++ R/PlantSim.R
@@ -8,7 +8,7 @@
 #' @param interaction_matrix The interaction matrix among species
 #' @param st_portion The portion of the seedlings that stay at the parental plot.
 #' @param surv_rate The survival rate for each plot and each species. It could be universally same.
-
+#' @param filesave The file name to be saved.
 #' @return The abundance matrix for all plots and species through time.
 #' @examples
 #' PlantSim(nplot = 5, nspe = 2, t = 10, ini_abundance = matrix(1:10, 5, 2),
@@ -23,12 +23,15 @@ PlantSim <-
            growth_rate,
            interaction_matrix,
            st_portion,
-           surv_rate) {
-    if (any(dim(ini_abundance) != c(nplot, nspe))) {
+           surv_rate,
+           filesave=NULL) {
+    if (is.matrix(ini_abundance) && all(dim(ini_abundance) == c(nplot, nspe))) {
+    } else {
       return (print(
-        "The length of the initial abundances is not equal to the number of species."
+        "Provide the initial abundances that is at the dim of c(nplot, nspe)."
       ))
     }
+
 
     if (is.null(dim(growth_rate))) {
       growth_rate <- matrix(growth_rate, nrow = nplot, ncol = nspe)
@@ -49,6 +52,12 @@ PlantSim <-
         )
       )
     }
+
+    if (dim(interaction_matrix)[1] != nspe) {
+      return(
+        print("Wrong dim for the interaction matrix.")
+      )
+    }
     # initialize the community matrix
     plot_abundance <- array(0, dim = c(nplot, nspe, t))
     plot_abundance[, , 1] <- ini_abundance
@@ -58,16 +67,23 @@ PlantSim <-
       for (plo in c(1:nplot)) {
         for (spe in c(1:nspe)) {
           new_seedlings[plo, spe] <-
-            plot_abundance[plo, spe, ts] * growth_rate[plo, spe] * exp(1 - plot_abundance[plo, , ts] %*% interaction_matrix[spe, ])
-        }
+            plot_abundance[plo, spe, ts] * growth_rate[plo, spe] * exp(1 - plot_abundance[plo, , ts] %*% interaction_matrix[spe,])
+          if (is.nan(new_seedlings[plo, spe])) {
+            print("Overflow numbers generated!")
+            break
+          }
+          }
       }
       update_seedlings <- matrix(0, nrow = nplot, ncol = nspe)
       for (plo in c(1:nplot)) {
-        update_seedlings[plo, ] <-
-          st_portion * new_seedlings[plo,] + (1 - st_portion) * (colSums(new_seedlings) - new_seedlings[plo, ]) / (nplot - 1)
+        update_seedlings[plo,] <-
+          st_portion * new_seedlings[plo, ] + (1 - st_portion) * (colSums(new_seedlings) - new_seedlings[plo,]) / (nplot - 1)
       }
 
       plot_abundance[, , ts + 1] <- surv_rate * update_seedlings
     }
+
+    if (is.null(filesave)){}
+    else {save(plot_abundance, file = filesave)}
     return(plot_abundance)
   }
